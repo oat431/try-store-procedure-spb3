5cc33d56a06dda5a01cf038142c6de3e40bbe49f
oat431 <oat431@gmail.com>
2023-09-25 01:07:06 +0700

    🔨 add: swagger for api documentation

--- src/main/java/panomete/practice/storepro/controller/RentalController.java
+++ src/main/java/panomete/practice/storepro/controller/RentalController.java
@@ -1,5 +1,11 @@
 package panomete.practice.storepro.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,16 +19,43 @@ import panomete.practice.storepro.service.RentalService;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/v1/rentals")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/rentals")
+@Tag(name = "Rental API", description = "All of the Rental API")
 public class RentalController {
     final RentalService rentalService;
 
+    @Operation(summary = "Get top 10 rentals")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful Operation",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = RentalDto.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/r-top10")
     public ResponseEntity<List<RentalDto>> getTop10Rentals() {
         return ResponseEntity.ok(rentalService.getTop10Rentals());
     }
 
+    @Operation(summary = "Get top 10 rentals by rate")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful Operation",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = RentalDto.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/top10")
     public ResponseEntity<List<RentalDto>> getTop10RentalsByRate(
             @RequestParam(name = "rate") RateType rate
@@ -30,6 +63,19 @@ public class RentalController {
         return ResponseEntity.ok(rentalService.getTop10Rentals(rate));
     }
 
+    @Operation(summary = "Get all rating")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful Operation",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = String.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/rating")
     public ResponseEntity<List<String>> getAllRating() {
         return ResponseEntity.ok(rentalService.getAllRating());
